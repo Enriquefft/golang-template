0d5c0467dd46d5aee0f2e91fe7647d4dc48c8d1a
Enriquefft <enriquefft2001@gmail.com>
2025-07-19 22:25:42 -0500

    added nix flakes

--- flake.nix
+++ flake.nix
@@ -1,14 +1,28 @@
 {
-  description = "Go project template";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-  outputs = { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.default = pkgs.mkShell {
-        buildInputs = [ pkgs.go pkgs.golangci-lint pkgs.gofumpt ];
-      };
+  description = "Go project template";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flakelight.url = "github:nix-community/flakelight";
+  };
+
+  outputs = { flakelight, nixpkgs, ... }:
+    flakelight ./. {
+
+      inputs.nixpkgs = nixpkgs;
+
+      devShell.packages = pkgs:
+        with pkgs; [
+
+          coreutils
+
+          lefthook
+
+          go
+          golangci-lint
+          gofumpt
+
+        ];
     };
+
 }
